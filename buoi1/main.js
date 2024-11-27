6138e361079c667b0ef36bf04617e51456c88dab
hctrung2k4 <hctrung2k4@gmail.com>
2024-11-27 22:01:13 +0700

    End Day 1

--- buoi1/main.js
+++ buoi1/main.js
@@ -1,5 +1,14 @@
 var string = "Hoang Chi Trung";
 var num = 10;
-prompt(string);
 
-setTimeout(function() {alert(string);}, 100);
+// var a = Math.random(); b = Math.random();
+// if (a>b)
+//     alert(a+' greater than ' +b);
+// else alert(a+' less than '+b);
+
+
+// var Array = ["C#","JavaScript","Java","NodeJS","C","C++","Java"]
+// console.log(Array)
+
+var Object = {name:"Chi Trung", age:20, school:"FPT University"};
+console.log(Object)
