258057532e348853852e8d74aef06ca3b3d23cc2
hctrung2k4 <hctrung2k4@gmail.com>
2024-11-28 16:59:56 +0700

    Day 2

--- buoi1/main.js
+++ buoi1/main.js
@@ -1,4 +1,4 @@
-var string = "Hoang Chi Trung";
+var string = "                 Hoang Chi Trung       " ;
 var num = 10;
 
 // var a = Math.random(); b = Math.random();
@@ -10,5 +10,10 @@ var num = 10;
 // var Array = ["C#","JavaScript","Java","NodeJS","C","C++","Java"]
 // console.log(Array)
 
-var Object = {name:"Chi Trung", age:20, school:"FPT University"};
-console.log(Object)
+// var Object = {name:"Chi Trung", age:20, school:"FPT University"}
+// var string = "Hoang Chi Trung Trung Trung";
+// console.log(string.indexOf('Trung',string.indexOf('Trung')+1) );
+// console.log(string.replace('Trung',"Hctrung"));
+// console.log(string.toUpperCase());
+string = string.trim();
+console.log(string)
